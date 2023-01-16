cd8f751fb8fe0e0b33ed9e899a79630b00b7f15b
Rayyouu <rayane.ainseba@ynov.com>
2023-01-16 16:19:28 +0100

    dotenv and db config

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require("express");
 var app = express();
+require("dotenv").config();
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("successfully connect to database");
+  })
+  .catch((err) => console.log(err));
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
@@ -7,4 +19,7 @@ app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
 });
 
-console.log("serveur")
+console.log("serveur");
+
+code =
+  "mongodb+srv://root:<password>@cluster0.h9d4ptt.mongodb.net/?retryWrites=true&w=majority";
